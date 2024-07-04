6e66769e9510de630d6000bacfb4c342a1908c83
Jaime de Venecia <deveneciaj@gmail.com>
2024-07-03 19:23:00 -0700

    add dropdown ui component, implement 'actions' on data table

--- src/components/admin-panel-table/columns.tsx
+++ src/components/admin-panel-table/columns.tsx
@@ -1,6 +1,18 @@
 'use client';
 
 import { ColumnDef } from '@tanstack/react-table';
+import { MoreHorizontal } from 'lucide-react';
+
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+
 import { Ticket } from '@/lib/types';
 
 export const columns: ColumnDef<Ticket>[] = [
@@ -23,5 +35,30 @@ export const columns: ColumnDef<Ticket>[] = [
   {
     accessorKey: 'supportTeamResponse',
     header: 'Support Team Response',
+  },
+  {
+    id: 'actions',
+    cell: ({ row }) => {
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant='ghost' className='h-8 w-8 p-0'>
+              <span className='sr-only'>Open menu</span>
+              <MoreHorizontal className='h-4 w-4' />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align='end'>
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            {/* <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(payment.id)}
+            >
+              Copy payment ID
+            </DropdownMenuItem> */}
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Respond</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
   },
 ];
