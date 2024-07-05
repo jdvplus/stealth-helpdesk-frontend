d6a939cc772a8c7bac622b2399f891386952d89d
Jaime de Venecia <deveneciaj@gmail.com>
2024-07-04 19:23:35 -0700

    update Dialog logic, refactor AdminPanel into client component for better UX

--- src/app/admin-panel/page.tsx
+++ src/app/admin-panel/page.tsx
@@ -1,11 +1,14 @@
+'use client';
+
 /*
 In a production-level application, this admin panel would of course be restricted with secure authentication measures, as the information is sensitive and should only visible to employees/team members/etc.
 
 (We could use https://next-auth.js.org/getting-started/example, for example.)
 */
 
-import { unstable_noStore as noStore } from 'next/cache';
+import { useState, useEffect } from 'react';
 
+import { Button } from '@/components/ui/button';
 import { DataTable } from '@/components/admin-panel-table/data-table';
 import { columns } from '@/components/admin-panel-table/columns';
 
@@ -13,20 +16,36 @@ import { Ticket } from '@/lib/types';
 
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
-const getData = async (): Promise<Ticket[]> => {
-  const res = await fetch(`${apiUrl}/tickets`);
-  const data = await res.json();
+export default function AdminPanel() {
+  const [data, setData] = useState(null);
 
-  return data;
-};
+  const getData = async (): Promise<void> => {
+    try {
+      const res = await fetch(`${apiUrl}/tickets`);
+      const data = await res.json();
 
-export default async function AdminPanel() {
-  noStore();
-  const data = await getData();
+      setData(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
-    <div className='w-[90%] mx-auto py-10'>
-      <DataTable columns={columns} data={data} />
+    <div className='w-[90%] mx-auto py-10 flex flex-col'>
+      <Button className='w-[10%] mx-auto mb-4' onClick={getData}>
+        refresh table
+      </Button>
+      {/* ^^ I really dislike this workaround for re-fetching data and re-rendering the table, but I unfortunately couldn't quite get server-side cache revalidation and page re-rendering with Next.js working properly given the assessment time constraints and the existing infrastructure of the codebase.
+
+      The actions of saving a draft and resolving a ticket occur from buttons within nested shadcn `Dialog` and `Dropdown Menu` components, which presented some complexity in terms of figuring out how to prop drill / manage state / etc. 
+      
+      This is certainly one of the downsides of relying on an external component library so heavily. */}
+
+      {data ? <DataTable columns={columns} data={data as Ticket[]} /> : null}
     </div>
   );
 }
--- src/components/actions-dropdown-w-response-modal.tsx
+++ src/components/actions-dropdown-w-response-modal.tsx
@@ -78,7 +78,12 @@ export default function ActionsDropDownWithResponseModal({
 
   /* component display */
   return (
-    <Dialog>
+    <Dialog
+      onOpenChange={() => {
+        setSampleEmail('');
+        setTicketResolved(false);
+      }}
+    >
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button variant='ghost' className='h-8 w-8 p-0'>
